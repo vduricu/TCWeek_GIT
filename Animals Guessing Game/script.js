4199a20eb933a70e5c4704e2aa24ca70d07143a3
Christine Moldovan <christine_moldovan@yahoo.com>
2022-06-22 13:16:26 +0300

    Worked on the Animal Guesser Game

---
+++ Animals Guessing Game/script.js
@@ -0,0 +1,42 @@
+const animals = ["Wolf", "Dog", "Cat", "Tiger", "Lion", "Panther", "Parrot", "Giraffe", "Dolphin", "Capybara"];
+const answer = animals[Math.floor(Math.random() * 100000 % animals.length)];
+let guesses = 0;
+
+function initGame() {
+    let hint = answer[0];
+    document.getElementById("UserGuesses").innerHTML = `Your hint is: ${hint}`;
+}
+
+initGame();
+
+function getGuess() {
+    let inputGuess = document.getElementById("UserInput").value;
+    document.getElementById("UserGuesses").innerHTML += `<br> ${document.getElementById("UserInput").value}`;
+
+    guesses++;
+
+    document.getElementById("myScore").innerHTML = guesses;
+
+    if (inputGuess !== answer) {
+        document.getElementById("myScore").innerHTML += `<br> ${inputGuess} is not the answer. Please try again!`;
+    }
+    else {
+        document.getElementById("myScore").innerHTML += `<br> ${inputGuess} is the correct answer. Well Done!`;
+        let playMore = prompt("Would you like to start a new game? (Y/N)");
+
+        if (playMore === 'Y') {
+
+        }
+    }
+}
+
+document.getElementById("SubmitData").addEventListener("click", getGuess);
+
+function getHint() {
+    let hintIndex = Math.floor(Math.random() * 100000 % animals.length);
+    let newHint = answer[hintIndex];
+
+    document.getElementById("UserGuesses").innerHTML += `<br> Your new hint is: ${newHint}`;
+}
+
+document.getElementById("AskHint").addEventListener("click", getHint);
